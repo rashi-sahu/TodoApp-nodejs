de5c80de39e3f93719ec9ad7d8aed6fc5980fe1b
rashi-sahu <rashi.sahu13@gmail.com>
2019-03-31 21:17:54 +0530

    Fetch data from mysql table

--- app.js
+++ app.js
@@ -4,19 +4,32 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "r"
+    password: "r",
+    database: 'todo'
   });
   
-  con.connect(function(err) {
+con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
+
   });
+
+
+
 var app = express();
 app.set('view-engine', 'ejs');
 app.use(express.static(__dirname+ '/views'));
 
+
 app.get('/todo', function(req,res){
-	res.render(__dirname + '/views/todo.ejs', { status: "success" });
+    var someResult;
+    con.query("SELECT * FROM todolist", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        // someResult = result;
+        res.render(__dirname + '/views/todo.ejs'  , {status : result[1].msg});
+    });
+	
 });
 
 app.listen(3000);
