f7bdc36a6252434e44d65fd3fd3c2404a64c0aee
rashi-sahu <rashi.sahu13@gmail.com>
2019-04-01 01:38:07 +0530

    Add post method to insert data into database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mysql = require('mysql');
+var bodyParser = require('body-parser');
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -19,6 +20,7 @@ con.connect(function(err) {
 var app = express();
 app.set('view-engine', 'ejs');
 app.use(express.static(__dirname+ '/views'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/todo', function(req,res){
@@ -27,9 +29,26 @@ app.get('/todo', function(req,res){
         if (err) throw err;
         console.log(result);
         // someResult = result;
-        res.render(__dirname + '/views/todo.ejs'  , {status : result[1].msg});
+        res.render(__dirname + '/views/todo.ejs'  , {status : result});
     });
 	
+});
+
+
+app.post('/myaction', function(req, res) {
+
+    console.log('req.body');
+    console.log(req.body);
+    res.write('You sent the name "' + req.body.name+'".\n');
+    res.write('You sent the Email "' + req.body.msg+'".\n');
+    
+    res.end()
+    
+    con.query("Insert into todolist (name,msg) VALUES ('"+req.body.name+"','"+req.body.msg+"')",function(err, result)      
+    {                                                      
+      if (err)
+         throw err;
+    });
 });
 
 app.listen(3000);
